4a987eefdd3af0041725db82dca468fbf57dc658
Amanda DiNoto <amanda.dinoto@gmail.com>
2023-05-18 18:23:35 -0400

    add description of problem for counting tickets

--- Counting Tickets/app.js
+++ Counting Tickets/app.js
@@ -1,3 +1,8 @@
+// You will be provided with an array of positive and negative integers representing a record of the comingss and goings of groups inside the park for the day 
+// Each integer represents the party size of a group either entering (positive) or exiting (negative) the park. Groups never split up or combine, and a valid record will never show a group leaving the park before they enter. 
+// In order to close the park for the day, you need the park to be empty. Write a function that returns 'CLOSE' if the park can safely close or 'OPEN' if there are still people in the park or the record is invalid.
+// Group sizes are not unique. There can be multiple parties of the same sie in the park.
+
 function getParkStatus(record) {
   let mapOfVisitors = new Map();
   let parkVisitors = 0;
